881d90671298988e08e1e10692af026d7344eae1
MinSang22Kim <powerminsang9901@gmail.com>
2024-07-14 18:37:56 +0900

    섹션8. 프록시와 연관관계 관리 - 즉시 로딩과 지연 로딩

--- src/main/java/jpabook/jpa/JpaMain.java
+++ src/main/java/jpabook/jpa/JpaMain.java
@@ -4,7 +4,11 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
+import jpabook.jpa.domain.Member;
 import jpabook.jpa.domain.Movie;
+import jpabook.jpa.domain.Team;
+
+import java.util.List;
 
 public class JpaMain {
 
@@ -17,13 +21,30 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Movie movie = new Movie();
-            movie.setDirector("A");
-            movie.setActor("BBB");
-            movie.setName("슈렉");
-            movie.setPrice(10000);
+            Team team1 = new Team();
+            team1.setName("teamA");
+            em.persist(team1);
 
-            em.persist(movie);
+            Team team2 = new Team();
+            team2.setName("teamB");
+            em.persist(team2);
+
+            Member member1 = new Member();
+            member1.setUsername("member1");
+            member1.setTeam(team1);
+
+            Member member2 = new Member();
+            member2.setUsername("member2");
+            member2.setTeam(team2);
+
+            em.flush();
+            em.clear();
+
+            List<Member> members = em.createQuery("select m from Member m", Member.class)
+                    .getResultList();
+
+            // SQL: select * from Member;
+            // SQL: select * from Team where TEAM_ID = xxx
 
             tx.commit();
         } catch (Exception e) {
--- src/main/java/jpabook/jpa/domain/Member.java
+++ src/main/java/jpabook/jpa/domain/Member.java
@@ -15,7 +15,7 @@ public class Member extends BaseEntity {
     @Column(name = "USERNAME")
     private String username;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "TEAM_ID", insertable = false, updatable = false)
     private Team team;
 
